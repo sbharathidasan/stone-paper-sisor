72be9599a30511ad7f9ead778f40d625d388bcd8
Bharathidasan S <128955313+sbharathidasan@users.noreply.github.com>
2023-12-20 22:31:42 +0530

    user interactive
    in the previous commit there is no user interaction in the code so we have updated it

--- spsci.py
+++ spsci.py
@@ -1,29 +1,39 @@
 import time  
 import random
-def countdown(c):      
-    while c:  
-        m, s = divmod(c, 60)  
-        timer = '{:02d}:{:02d}'.format(m, s)  
-        print(timer, end="\r")  
-        time.sleep(1)  
-        c -= 1       
-def spsci():
-    d=random.randint(1,3)
-    if d==1:
-        print("stone")
-    elif d==2:
-        print("paper")
+def inisi():
+    i=input("choose \"stone\" , \"paper\" or \"scissor\" ")
+    print("my decission is")
+    des = ["scissor", "paper", "stone"]
+    d=random.randint(0,2)
+    if(d==0):
+        m=des[d]
+        print(m)
+    elif(d==1):
+        m=des[d]
+        print(m)
     else:
-        print("scissor")
+        m=des[d]
+        print(m)
+    spsci(m,i)
+def spsci(m,i):
+    if m==i:
+        print("match draw")
+        inisi()
+    else:
+        if m=="stone" and i=="paper":
+            print("you won")
+        elif m=="paper" and i=="scissor":
+            print("you won")
+        elif m=="scissor" and i=="stone":
+            print("you won")
+        else :
+            print("better luck next time")
 def timfu():
-    print("choose \"stone\" , \"paper\" or \"scissor\" in mind")
     c=2
     while c:
         m, s = divmod(c, 60)
         timer = '{:02d}:{:02d}'.format(m, s)
         time.sleep(1)
         c -= 1
+inisi()
 timfu()
-countdown(5)
-print("my decission is")
-spsci()  
